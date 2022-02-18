ef8ab53c39fc898c6d3d33e246566242fe47c3be
pk <prakashpandey00002@gmail.com>
2022-02-19 00:04:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (35.03%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -1,7 +1,18 @@
 class Solution {
 public:
     double angleClock(int hour, int minutes) 
-    {
+    { 
+          /* we know that hour hand makes 360 degree in 12hrs so that in 1 minutes
+          it make 360/12*60=0.5 degree in 1 minutes
+          
+          just like hour hand ,minutes hand makes 360/60=6 degree in one minutes
+          
+           calculate difference and take min of both angle 
+           
+            constraints is so easy that minutes hand <=59minutes 
+            
+            understandble code is below*/
+         
          double hour_angle=(hour*60+minutes)*0.5;
         double minutes_angle=(minutes*6);
         
