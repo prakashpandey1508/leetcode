e1b0bbfd91db4402eb6f5ea12adf8fb739164be7
pk <prakashpandey00002@gmail.com>
2022-03-16 01:03:05 +0530

    Time: 19 ms (92.61%), Space: 11.7 MB (49.65%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) 
+    {
+        string ans="";
+        stack<int>st;
+        int n=s.size();
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]=='(')
+                st.push(i);
+            else if(s[i]==')')
+            {
+                if(st.empty())
+                {
+                    s[i]='*';
+                    
+                }
+                else
+                    st.pop();
+            }
+        }
+        while(!st.empty())
+        {
+            s[st.top()]='*';
+            st.pop();
+        }
+        for(int i=0;i<n;i++)
+        {
+            if(s[i]!='*')
+                ans+=s[i];
+        }
+        return ans;
+    }
+};
