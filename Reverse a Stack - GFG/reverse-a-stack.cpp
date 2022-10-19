316dce74fe223d2d6c84439fb1153c792b5ae745
pk <prakashpandey00002@gmail.com>
2022-10-19 18:08:54 +0530

    Added solution - LeetHub

---
+++ Reverse a Stack - GFG/reverse-a-stack.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+public:
+     void insertatbuttom(stack<int>&s,int x)
+     {
+         if(s.empty())
+         {
+             s.push(x);
+             return ;
+             
+         }
+         int y=s.top();
+         s.pop();
+         insertatbuttom(s,x);
+         s.push(y);
+     }
+    void Reverse(stack<int> &St)
+    {
+           if(St.empty())
+           {
+               return ;
+           }
+           int x=St.top();
+           St.pop();
+           Reverse(St);
+           insertatbuttom(St,x);
+    }
+};
+
+//{ Driver Code Starts.
+
+
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int N;
+        cin>>N;
+        stack<int> St;
+        for(int i=0;i<N;i++){
+            int x;
+            cin>>x;
+            St.push(x);
+        }
+        Solution ob;
+        ob.Reverse(St);
+        vector<int>res;
+        while(St.size())
+        {
+            res.push_back(St.top());
+            St.pop();
+        }
+        for(int i = res.size()-1;i>=0;i--)
+        {
+            cout<<res[i]<<" ";
+        }
+        
+        cout<<endl;
+    }
+}
+// } Driver Code Ends
